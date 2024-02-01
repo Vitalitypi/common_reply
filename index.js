be0cc4a1f91320652cb285ffe3803085065ed0e4
Vitalitypi <779958365@qq.com>
2024-02-01 11:28:22 +0800

    test name

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Namer } = require("./db");
+const mysql = require('mysql');
+const { init: initDB, Counter } = require("./db");
+// 从环境变量中读取数据库配置
+const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const [host_mysql, port_mysql] = MYSQL_ADDRESS.split(":");
 
 const logger = morgan("tiny");
 
 const app = express();
+// 创建mysql连接
+const connection = mysql.createConnection({
+  host: host_mysql,
+  port: port_mysql,
+  user: MYSQL_USERNAME,
+  password: MYSQL_PASSWORD,
+  database: 'nodejs_demo'
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database');
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -16,26 +35,36 @@ app.use(logger);
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-
+// 创建路由获取test表的name字段
+app.get('/test', (req, res) => {
+  connection.query('SELECT name FROM test', (error, results, fields) => {
+    if (error) {
+      console.error('Error querying database: ' + error.stack);
+      res.status(500).send('Error querying database');
+      return;
+    }
+    res.json(results);
+  });
+});
 // 更新计数
 app.post("/api/count", async (req, res) => {
   const { action } = req.body;
   if (action === "inc") {
-    await Namer.create();
+    await Counter.create();
   } else if (action === "clear") {
-    await Namer.destroy({
+    await Counter.destroy({
       truncate: true,
     });
   }
   res.send({
     code: 0,
-    data: await Namer.name(),
+    data: await Counter.count(),
   });
 });
 
 // 获取计数
 app.get("/api/count", async (req, res) => {
-  const result = await Namer.name();
+  const result = await Counter.count();
   res.send({
     code: 0,
     data: result,
